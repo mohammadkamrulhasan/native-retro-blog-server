4635337021e3e84c3de7af1143685b3c352e54f9
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-06-27 00:06:21 +0600

    Update model and route

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const cors=require('cors');
 const bodyParser=require('body-parser');
-require('dotenv').config()
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+
+
+require('dotenv').config();
 const port =process.env.PORT || 5055 ;
 // console.log(process.env.DB_USER);
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
 
 app.get('/', (req, res)=>{
     res.send('Assalamu Alaikum')
@@ -26,6 +44,25 @@ client.connect(err => {
  
 });
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+  
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  app.use("/api/posts", postRoute);
+  app.use("/api/categories", categoryRoute);
+  
 
 
 app.listen(process.env.PORT || port)
